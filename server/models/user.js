8cd9eaab0508eeeab51523c5895d7c8808fd906a
Alexander Gozali <alexandergozali0@gmail.com>
2023-02-07 19:47:46 +0700

    Feat: User and Profile Models

---
+++ server/models/user.js
@@ -0,0 +1,32 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+    class User extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+            User.hasOne(models.Profile, {
+                foreignKey: "user_id",
+            });
+        }
+    }
+    User.init(
+        {
+            username: DataTypes.STRING,
+            email: DataTypes.STRING,
+            password: DataTypes.STRING,
+            role: {
+                type: DataTypes.ENUM("admin", "user"),
+            },
+        },
+        {
+            sequelize,
+            modelName: "User",
+        }
+    );
+    return User;
+};
